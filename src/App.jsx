f408ab7b6c4056269891415008210c294bf982dc
mbelmontes7 <you@example.com>
2024-05-15 21:35:58 -0500

    chore: Add ButtonGradient component and update App layout learning how to make resuable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import ButtonGradient from "./assets/svg/ButtonGradient"; //always check the import bro fucking shit 
+import Button from "./components/Button";
 
 
 const App = () => {
   return (
+    <>
     <h1 className="text-3xl font-bold underline text-cyan-600">
-      Hello world!
-    </h1>
+      Hello world!</h1>
+     <div className="pt-[4.75rem] lg:pt[5.25rem] overflow-hidden">
+      <Button className="mt-10">Something</Button>
+ 
+    </div>
+    <ButtonGradient /> 
+    </>
   )
 }
 
