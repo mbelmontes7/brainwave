d3f48a4b89d884f2d61d0a22e5a452ed04293939
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-01 22:48:31 -0500

    refactor: Improve Collaboration component layout and add lazy loading for images

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -38,16 +38,17 @@ const Collaboration = () =>
                 </div>
 
                 <div className="lg:ml-auto xl:w-[38rem] mt-4">
-                    <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto"> 
+                    <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
                         {collabText}
                     </p>
                     {/* //In here you can see the outer most circle of the code */}
-                        <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+                    <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
                         <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
                             <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
                                 <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                                    {/* //this is the images for the brainwave symbol with in inside this div we are inside the circle that is showing all the icons*/}
                                     <img
-                                        src={brainwaveSymbol} 
+                                        src={brainwaveSymbol}
                                         width={48}
                                         height={48}
                                         alt="brainwave"
