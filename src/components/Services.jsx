e87d477273776be2f8145a349da5f760eb8d977c
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-03 19:01:14 -0500

    feat: Add Smartest AI section to Services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -16,7 +16,6 @@ const Services = () =>
             <div className="relative">
                 <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
                     <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
-
                         <img
                             className="w-full h-full object-cover md:object-right"
                             width={800}
@@ -24,6 +23,12 @@ const Services = () =>
                             height={730}
                             src={service1}
                         />
+                    </div>
+                    <div className="relative z-1 max-w-[17rem] ml-auto">
+                        <h4 className="h4 mb-4">Smartest AI</h4>
+                        <p className="body-2 mb-[3rem] text-n-3">
+                            Brainwave unlocks the potential of AI-powered applications
+                        </p>
                     </div>
                 </div>
             </div>
