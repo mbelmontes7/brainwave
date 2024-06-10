77c562d038b7d0c7ab6720dc14368e4f55e46153
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-10 18:09:06 -0500

    Created the border for the rounted pricing baorder inside there is the text with the color.

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -7,12 +7,20 @@ const PricingList = () =>
   return (
     // A div container with flexbox layout, spacing between items of 1rem,
     // and wrapping of items when the screen size is below the "lg" breakpoint
-     <div className="flex gap-[1rem] max-lg:flex-wrap">
+    <div className="flex gap-[1rem] max-lg:flex-wrap">
+      {/* // Mapping over the pricing array to create a new div for each item */}
       {pricing.map((item) => (
+        // A div for each pricing item with specific styles:
+        // width of 19rem, full width on screens smaller than "lg",
+        // full height, padding on x-axis of 6, background color of n-8,
+        // border color of n-6, rounded corners of 2rem,
+        // additional styles for large screens, and different padding and margin for even/odd items
         <div
           key={item.id}
           className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3"
         >
+          {/* // A header element (h4) displaying the title of the pricing item,
+      // with specific margin at the bottom and class for styling */}
           <h4 className="h4 mb-4">{item.title}</h4>
 
           <p className="body-2 min-h-[4rem] mb-3 text-n-1/50">
