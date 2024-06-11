4d35eb2777a7c64231f2947120bdcbe5fca4ceaf
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-11 11:50:03 -0500

    Added the roadmap now I'll break down the code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 
 
 const App = () =>
@@ -22,6 +23,7 @@ const App = () =>
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap/>
 
       </div>
       <ButtonGradient />
